94962f074e47abed15ef1ce0265cb2e8bdf08888
M C <martin.crabtree@gmail.com>
2022-07-26 19:57:25 -0400

    set up base imports and functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("test")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+// temp data arr
+const ads = [
+  {title: 'Hello, world (again)!'}
+];
+
+// Helmet for API security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(ads);
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
