5678af28014168b1ab70debca0d5224881c4bd68
M C <martin.crabtree@gmail.com>
2022-08-02 20:05:25 -0400

    adding dummy data to test get requests

--- src/index.js
+++ src/index.js
@@ -7,10 +7,30 @@ import morgan from 'morgan';
 
 const app = express();
 
-// temp data arr
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
+// temp data arr users and messages
+let users = {
+  1: {
+    id: '1',
+    username: 'Thomas',
+  },
+  2: {
+    id: '2',
+    username: 'Dave',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Text for user id 1',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Text for user id 2',
+    userId: '2',
+  },
+};
 
 // Helmet for API security
 app.use(helmet());
@@ -24,20 +44,31 @@ app.use(cors());
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
+
+
+
 app.get('/users', (req, res) => {
-  return res.send('Received a GET HTTP method');
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
   return res.send('Received a POST HTTP method');
 });
 
-app.put('/users', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
 });
 
-app.delete('/users', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
 });
 
 app.listen(process.env.PORT, () =>
