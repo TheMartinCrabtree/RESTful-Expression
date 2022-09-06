028ec7d91a7664503a0990f3e09abf3e63f62788
M C <martin.crabtree@gmail.com>
2022-09-05 21:49:17 -0400

    refactored and changed method for uuid

--- src/index.js
+++ src/index.js
@@ -1,55 +1,47 @@
 import 'dotenv/config';
+import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid'; 
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import morgan from 'morgan';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
 
-// temp data arr users and messages
 let users = {
   1: {
     id: '1',
-    username: 'Thomas',
+    username: 'Bob Villa',
   },
   2: {
     id: '2',
-    username: 'Dave',
+    username: 'Bob Ross',
   },
 };
 
 let messages = {
   1: {
     id: '1',
-    text: 'Text for user id 1',
+    text: 'Hello World',
     userId: '1',
   },
   2: {
     id: '2',
-    text: 'Text for user id 2',
+    text: 'Bye World',
     userId: '2',
   },
 };
 
-// Helmet for API security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
-
-
-
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
 
 app.get('/users', (req, res) => {
   return res.send(Object.values(users));
@@ -67,37 +59,30 @@ app.get('/messages/:messageId', (req, res) => {
   return res.send(messages[req.params.messageId]);
 });
 
-app.post('/users', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
-);
-
-
 app.post('/messages', (req, res) => {
   const id = uuidv4();
   const message = {
     id,
+    text: req.body.text,
+    userId: req.me.id,
   };
-  
-  const date = Date.parse(req.body.date);
-  const count = Number(req.body.count);
 
   messages[id] = message;
 
   return res.send(message);
 });
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
